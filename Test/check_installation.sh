eeb1a446c759ec77359e2ed6c87c7fc9b3b5cc0b
levypan <levy-pan@outlook.com>
2021-07-11 21:48:27 +0800

    update on 20210711 21:48

---
+++ Test/check_installation.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+###
+ # @Description  :
+ # @Author       : jsmjsm
+ # @Github       : https://github.com/jsmjsm
+ # @Date         : 2021-07-11 21:42:27
+ # @LastEditors  : jsmjsm
+ # @LastEditTime : 2021-07-11 21:47:59
+ # @FilePath     : /mac-setup/Test/check_installation.sh
+###
+
+# > Installed Package Checking
+install() {
+    echo "Debug: install Begin"
+    check_installation $1
+    if [[ $? -eq 0 ]]; then
+        echo "👌 ==> Installed" $1 ", skip..."
+    else
+        echo "🔥 ==> Installing " $1
+        echo "Debug: Running brew install "
+    fi
+}
+
+check_installation() {
+    # if [[ $type == "cli" ]]; then
+    brew list -1 | grep $1 > /dev/null
+
+    if [[ $? -eq 0 ]]; then
+        # not installed, return 0
+        return 0
+    fi
+
+    # installed, return 1
+    return 1
+}
+
+install $1
+
+exit
+
